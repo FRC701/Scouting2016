d5a627e99ac25be77959ba75db1f6748680d2d59
BA313 <benaldrich2000@gmail.com>
2016-01-23 11:27:47 -0800

    adding database

---
+++ pyTK 2016/model/entry.py
@@ -0,0 +1,137 @@
+#------------------------------------------------------------------------------
+# entry module
+#   -- makes sense of the data collected
+#------------------------------------------------------------------------------
+from stack import *
+
+#------------------------------------------------------------------------------
+# Entry class
+#   -- Equivalent to a single ms-access entry
+#   -- each match has 6 of these entries
+#------------------------------------------------------------------------------
+class Entry(object):
+    """Pull in loaded data and sort it to be later assigned to team values."""
+
+    entries = [] # list holding all the entries, 6 per match
+    
+    def __init__(self, data):
+        # general info
+        index = 0
+        self.match = int(data[index])
+        index += 1
+        self.team = int(data[index])
+        index += 1
+        self.allianceColor = int(data[index])
+        index += 1
+        self.noShow = bool(int(data[index]))
+        
+        
+        # autonomous data
+        self.autoHadAuto = bool(int(data[index]))
+        index += 1
+        self.autoReachesDefences = bool(int(data[index]))
+        index += 1
+        self.autoCrossesDefences = bool(int(data[index]))
+        index += 1
+        self.autoDefences1 = int(data[index]))
+        index += 1
+        self.autoDefences2 = int(data[index]))
+        index += 1
+        self.autoBouldersInLowGoal = float(data[index])
+        index += 1
+        self.autoBouldersInHighGoal = float(data[index])
+        index += 1
+        self.autoOther = bool(int(data[index]))
+        
+
+        # tele-op data
+        self.teleLowBar = str(data[index]))
+        index +=1
+        self.teleDamageCounter1 = float(data[inde]))
+        index +=1
+        self.teleDefences1 = str(data[index]))
+        index +=1
+        self.teleDamageCounter2 = float(data[index]))
+        index +=1
+        self.teleDefences2 = str(data[index]))
+        index +=1
+        self.teleDamageCounter3 = float(data[index]))
+        index +=1
+        self.teleDefences3 = str(data[index]))
+        index +=1
+        self.teleDamageCounter4 = float(data[index]))
+        index +=1
+        self.teleDefences4 = str(data[index]))
+        index +=1
+        self.teleDamageCounter5 = float(data[index]))
+        index +=1
+        self.teleBouldersInLowGoal = float(data[index]))
+        index +=1
+        self.teleBouldersInHighGoal = float(data[index]))
+        index +=1
+        self.teleBouldersFromLowGoal = float(data[index]))
+        index +=1
+        self.teleBouldersFromHighGoal = float(data[index]))
+         
+        # post data
+        self.postFouls = float(data[index])
+        index += 1
+        self.postTechFouls = float(data[index])
+        index += 1
+        self.postRedCard = bool(int(data[index]))
+        index += 1
+        self.postYellowCard = bool(int(data[index]))
+        index += 1
+        self.postDisabled = bool(int(data[index]))
+        index += 1
+        self.postPlayedDefensively = bool(int(data[index]))
+        index += 1
+        self.postCaptured = bool(int(data[index]))
+        index += 1
+        self.postBreached = bool(int(data[index]))
+        index += 1
+        self.postChallengeState = int(data[index]))
+        index += 1
+        self.postScaleState = int(data[index]))
+        index += 1
+
+        self.entries.append(self)
+    def sort(self):
+        """Calculates basic scoring and information."""
+            
+        self.avgTeleBouldersInLowGoal = float(sum(self.teleBouldersInLowGoal))/float(len(self.teleBouldersInLowGoal)) if len(self.BouldersInLowGoal) else 0
+        self.avgTeleBouldersInHighGoal = float(sum(self.teleBouldersInHighGoal))/float(len(self.teleBouldersInHighGoal)) if len(self.teleBouldersInHighGoal) else 0
+        self.avgTeleBouldersFromLowGoal = float(sum(self.teleBouldersFromLowGoal))/float(len(self.teleBouldersFromLowGoal)) if len(self.teleBouldersFromLowGoal) else 0
+        self.avgTeleBouldersFromHighGoal = float(sum(self.teleBouldersFromHighGoal))/float(len(self.teleBouldersFromHighGoal)) if len(self.teleBouldersFromHighGoal) else 0
+
+        self.autoReachesDefences = 2 if self.autoReachesDefences = 1 else self.autoReachesDefences = 0
+
+        self.autoCrossesDefences = 10 if self.autoDefences1 >=1 and self.autoDefences2 = 0 elif self.autoDefences2 >= 1 and self.autoDefences = 0 else self.autoCrossesDefences = 0
+        self.autoCrossesDefences = 20 if self.autoDefences1 >=1 and self.autoDefences2 >= 1 else self.autoCrossesDefences = 0
+
+        self.autoLowGoal = (self.autoBouldersInLowGoal*5)
+        self.autoHighGoal = (self.autoBouldersInHighGoal*10)
+
+        self.autoScore = (self.autoReachesDefences + self.autoCrossesDefences + self.autoLowGoal + self.autoHighGoal)
+        
+        self.teleDC1 = (self.teleDamageCounter1*5) 
+        self.teleDC2 = (self.teleDamageCounter2*5) 
+        self.teleDC3 = (self.teleDamageCounter3*5) 
+        self.teleDC4 = (self.teleDamageCounter4*5) 
+        self.teleDC5 = (self.teleDamageCounter5*5)
+        
+        self.teleDefencesScore = (self.teleDC1 + self.teleDC2 + self.teleDC3 + self.teleDC4 + self.teleDC5)
+
+        self.teleLowGoal = (self.teleBouldersInlowGoal*2)
+        self.teleHighGoal = (self.teleBouldersInHighGoal*5)
+        
+        self.teleScore = (self.teleDefencesScore + self.teleLowGoal + self.teleHighGoal)
+
+        self.scoredInAuto = True if self.autoScore > 0 else False
+        self.scoredInTele = True if self.teleScore > 0 else False
+        self.hasFoul      = True if self.postFouls > 0 elif self.postTechFouls > 0 elif self.postFouls > 0 and self.postTechFouls > 0 else False
+
+        self.offensiveScore = (self.autoScore + self.teleScore)
+        self.foulScore = (5*self.postFouls + 5*self.postTechFoul)
+
+        self.totalScore = (self.offensiveScore - self.foulScore)
---
+++ pyTK 2016/model/team.py
@@ -0,0 +1,210 @@
+#------------------------------------------------------------------------------
+# team Module
+#   -- Keeps track of valuable team information and scorings
+#------------------------------------------------------------------------------
+
+
+#------------------------------------------------------------------------------
+# TeamInfo Class
+#   -- Stores performance information
+#------------------------------------------------------------------------------
+class _TeamInfo(object):
+    """Used to handle information for different teams."""
+
+    def __init__(self):
+        self.matches = []                   # list holding the matches the team was in
+        self.noShow = 0                     # number of matches for which the team didn't show up
+        
+        self.autoHadAuto = 0                # number of matches for which the team had autonomous
+        self.autoReachesDefences = 0
+        self.autoCrossesDefences = 0
+        self.autoDefences1 = []
+        self.autoDefences2 = []
+        self.autoBouldersInLowGoal = []
+        self.autoBouldersInHigh = []
+        self.autoOther = 0                  # number of matches for which the team did something else in auto
+
+        self.teleLowBar = []
+        self.teleDamageCounter1 = []
+        self.teleDefences1 = []
+        self.teleDamagecounter2 = []
+        self.teleDefences2 = []
+        self.teleDamageCounter3 = []
+        self.teleDefences3 = []
+        self.teleDamageCounter4 = []
+        self.teleDefences4 = []
+        self.teleDamageCounter5 = []
+        self.teleBouldersInLowGoal = []
+        self.teleBouldersInHighGoal = []
+
+        self.postFouls = []                 # list containing the number of fouls each match
+        self.postTechFouls = []
+        self.postRedCard = 0                # number of matches for which robot received red card
+        self.postYellowCard = 0             # number of matches for which robot received yellow card
+        self.postDisabled = 0               # number of matches for which robot was disabled
+        self.postplayedDefensively = 0
+        self.postCaptured = 0
+        self.postBreached = 0
+        self.postChallengeState = []
+        self.postScaleState = []
+
+        self.scoredInTele = 0               # number of matches for which the robot scored in tele-op
+        self.scoredInAuto = 0               # number of matches for which the robot scored in auto
+        self.hasFoul = 0                    # number of matches for which the robot received a foul
+
+    def get_info(self):
+        self.avgAutoBoulderInLowGoal = float(sum(self.autoBouldersInLowGoal))/float(len(self.autoBouldersInLowGoal)) if len(self.autoBouldersInLowGoal) else 0
+        self.avgAutoBouldersInHighGoal = float(sum(self.autoBoulderInHighGoal))/float(len(self.autoBouldersInHighGoal)) if len(self.autoBouldersInHIghGoal) else 0
+
+        self.avgTeleBouldersInLowGoal = float(sum(self.teleBouldersInLowGoal))/float(len(self.teleBouldersInLowGoal)) if len(self.teleBouldersInLowGoal) else 0
+        self.avgTeleBouldersInHighGoal = float(sum(self.teleBOuldersInHighGoal))/float)(len(self.teleBouldersInHighGoal)) if len(self.teleBouldersInHighGoal) else 0
+        
+        self.avgPostFoul = sum(self.postFouls)/len(self.postFouls) if len(self.postFouls) else 0
+        self.avgPostTechFoul = sum(self.postTechFoul)/len(self.postTechFouls) if len(self.postTechFouls) else 0 
+                
+    def getAttr(self, source):
+        return getattr(self, source)
+
+#------------------------------------------------------------------------------
+# TeamScores Class
+#   -- stores data about a team's scores
+#------------------------------------------------------------------------------
+class _TeamScores(object):
+    """Used to handle scoring data for different teams."""
+
+    def __init__(self):
+        self.oScores = []               # list holding offensive scores
+        self.tScores = []               # list holding total scores
+        self.autoScores = []            # list holding auto scores
+        self.autoRobotScores = []       # list holding auto robot scores
+        self.autoBouldersInLowGoal = []
+        self.autoBouldersInHighGoal = []
+        self.teleScores = []            # list holding tele scores
+        self.teleBouldersInLowGoal = []
+        self.teleBouldersInHighGoal = []
+        self.foulScores = []            # list holding foul scores
+        
+    def get_maxmin_scores(self):
+        self.maxOffScore = max(self.oScores)
+        self.minOffScore = min(self.oScores)
+        self.maxTotalScore = max(self.tScores)
+        self.minTotalScore = min(self.tScores)
+        self.maxAutoScore = max(self.autoScores)
+        self.minAutoScore = min(self.autoScores)
+        self.maxAutoBouldersInLowGoal = max(self.autoBoulderInLowGoal)
+        self.minAutoBouldersInLowGoal = min(self.autoBoulderInLowGoal)
+        self.maxAutoBouldersInHighGoal = max(self.autoBouldersInHighGoal)
+        self.minAutoBouldersInHighGoal = min(self.autoBouldersInHighGoal)
+        self.maxAutoRobotScore = max(self.autoRobotScores)
+        self.minAutoRobotScore = min(self.autoRobotScores)
+        self.maxTeleScore = max(self.teleScores)
+        self.minTeleScore = min(self.teleScores)
+        self.maxTeleBouldersInLowGoal = max(self.teleBouldersInLowGoal)
+        self.minTeleBouldersInLowGoal = min(self.teleBouldersInLowGoal)
+        self.maxTeleBouldersInHighGoal = max(self.teleBouldersInHighGoal)
+        self.minTeleBouldersInHighGoal = min(self.teleBouldersInHighGoal)
+        self.maxFoulScore = max(self.foulScores)
+        self.minFoulScore = min(self.foulScores)
+
+    def get_avg_scores(self, matches=1, auto=0):
+        self.avgOffScore = sum(self.oScores)/matches if matches else 0
+        self.avgAutoScore = sum(self.autoScores)/auto if auto else 0
+        self.avgAutoBouldersInLowGoal = sum(self.autoBouldersInLowGoal)/auto if auto else 0
+        self.avgAutoBouldersInHighGoal = sum(self.autoBouldersInHighGoal)/auto if auto else 0
+        self.avgAutoRobotScore = sum(self.autoRobotScores)/auto if auto else 0
+        self.avgTeleScore = sum(self.teleScores)/matches if matches else 0
+        self.avgTeleBouldersInLowGoal = sum(self.teleBouldersInLowGoal)/matches if matches else 0
+        self.avgTeleBouldersInHighGoal = sum(self.teleBouldersInHighGoal)/matches if matches else 0
+        self.avgFoulScore = sum(self.foulScores)/matches if matches else 0
+        self.avgTotalScore = sum(self.tScores)/matches if matches else 0
+
+    def getAttr(self, source):
+        return getattr(self, source)
+
+#------------------------------------------------------------------------------
+# TeamRankings class
+#   -- place to store ranking lists, for viewing team ranks
+#------------------------------------------------------------------------------
+class TeamRankings(object):
+    """Used to keep track of rankings for each team."""
+
+    off_rank = []
+    auto_rank = []
+    auto_Low_Goal_rank = []
+    auto_High_Goal_rank = []
+    auto_robot_rank = []
+    tele_rank = []
+    tele_Low_Goal_rank = []
+    tele_High_Goal_rank = []
+    tele_litter_rank = []
+    foul_rank = []
+    tot_rank = []
+    
+    def __init__(self):
+        print
+        # no non-static class variables
+        # team cannot track its own ranking:
+            # rankings are defined by the user
+            # rankings are dynamic, constantly changing to user request
+
+    def getAttr(self, source):
+        return getattr(self, source)
+
+#------------------------------------------------------------------------------
+# Team Class
+#   -- stores and recalls team specific data
+#------------------------------------------------------------------------------
+class Team(object):
+    """Store and recall data on a team from here."""
+
+    team_list = []  # list holding all the teams currently loaded in the database
+    available = []  # list holding all the teams not currently selected
+    wanted = []     # list holding all the teams in our wanted list
+    
+    def __init__(self, num):
+        self.number = num
+        self.Info = _TeamInfo()
+        self.Scores = _TeamScores()
+        self.team_list.append(self)
+        self.available.append(self)
+
+        # a few of the final details predefined so as to satisfy predictions with null teams
+        self.avgOff = 0
+        self.pOff = 0
+        
+    def get_details(self): # gets all of the information for the team
+        self.Info.get_info()
+        self.Scores.get_avg_scores(len(self.Info.matches),self.Info.autoHadAuto)
+        self.Scores.get_maxmin_scores()
+
+        matches = self.Info.matches
+        self.numMatch = len(matches)
+        self.pNoShow  = str(int(100*self.Info.noShow)/len(matches)) + "%"
+        self.pDisabled = str(int(100*self.Info.postDisabled)/len(matches)) + "%"
+        self.avgOff = round(self.Scores.avgOffScore,2)
+        self.avgTotal = round(self.Scores.avgTotalScore,2)
+
+        self.pHadAuto = str(int(100*self.Info.autoHadAuto)/len(matches)) + "%"
+        self.pReachesDefences = str(int(100*self.Info.autoReachesDefences)/len(matches)) + "%"
+        self.pCrossesDefences = str(int(100*self.Info.autoCrossesDefences)/len(matches)) + "%"
+        self.avgAutoScore = round(self.Scores.avgAutoScore,2)
+        self.avgAutoBouldersInLowGoal = round(self.Scores.avgAutoBouldersInLowGoal,2)
+        self.avgAutoBouldersInHighGoal = round(self.Scores.avgAutoBouldersInHighGoal,2)
+        self.avgAutoRobotScore = round(self.Scores.avgAutoRobotScore,2)
+        self.pAutoOther = str(int(100*self.Info.autoOther)/len(matches)) + "%"
+        
+        self.avgTeleScore = round(self.Scores.avgTeleScore,2)
+        self.avgTeleBouldersInLowGoal = round(self.Scores.avgTeleBouldersInLowGoal,2)
+        self.avgTeleBouldersInHighGoal = round(self.Scores.avgTeleBouldersInHighGoal,2)
+       
+        self.avgFoulScore = round(self.Scores.avgFoulScore,2)
+        self.avgPostFoul = round(self.Info.avgPostFoul,2)
+        self.pFoul = str(int(100*self.Info.hasFoul)/len(matches)) + "%"
+        self.pYellow = str(int(100*self.Info.postYellowCard)/len(matches)) + "%"
+        self.pRed = str(int(100*self.Info.postRedCard)/len(matches)) + "%"
+        self.pPlayedDefensively = str(int(100*self.Info.postPlayedDefensively)/len(matches)) + "%"
+        self.pCaptured = str(int(100*self.Info.postCaptured)/len(matches)) + "%"
+        self.pBreached = str(int(100*self.Info.postBreached)/len(matches)) + "%"
+
+    def getAttr(self, source):
+        return getattr(self, source)

bcdcfed442d6eb6216d9ebdd585bf20cf39a5c8c
BA313 <benaldrich2000@gmail.com>
2016-02-01 18:42:57 -0800

    FINIALLY!!!
    entry data formate fixes

--- pyTK 2016/model/entry.py
+++ pyTK 2016/model/entry.py
@@ -26,13 +26,12 @@ class Entry(object):
         self.noShow = bool(int(data[index]))
         index += 1
         
-        """
         # autonomous data
-        self.autoHadAuto = bool(int(data[index]))
+        self.autoHadAuto = bool(data[index])
         index += 1
-        self.autoReachesDefences = bool(int(data[index]))
+        self.autoReachesDefences = bool(data[index])
         index += 1
-        self.autoCrossesDefences = bool(int(data[index]))
+        self.autoCrossesDefences = bool(data[index])
         index += 1
         self.autoDefences1 = int(data[index])
         index += 1
@@ -42,27 +41,28 @@ class Entry(object):
         index += 1
         self.autoBouldersInHighGoal = float(data[index])
         index += 1
-        self.autoOther = bool(int(data[index]))
+        self.autoOther = bool(data[index])
         index += 1
 
+        
         # tele-op data
-        self.teleLowBar = str(data[index])
+        self.teleLowBar = (data[index])
         index +=1
         self.teleDamageCounter1 = float(data[index])
         index +=1
-        self.teleDefences1 = str(data[index])
+        self.teleDefences1 = (data[index])
         index +=1
         self.teleDamageCounter2 = float(data[index])
         index +=1
-        self.teleDefences2 = str(data[index])
+        self.teleDefences2 = (data[index])
         index +=1
         self.teleDamageCounter3 = float(data[index])
         index +=1
-        self.teleDefences3 = str(data[index])
+        self.teleDefences3 = (data[index])
         index +=1
         self.teleDamageCounter4 = float(data[index])
         index +=1
-        self.teleDefences4 = str(data[index])
+        self.teleDefences4 = (data[index])
         index +=1
         self.teleDamageCounter5 = float(data[index])
         index +=1
@@ -95,19 +95,13 @@ class Entry(object):
         self.postChallengeState = int(data[index])
         index += 1
         self.postScaleState = int(data[index])
-        """
+        
 
         self.entries.append(self)
         
     def primary_sort(self):
         """Calculates basic scoring and information."""
-        self.avgAutoBouldersInLowGoal = float(sum(self.autoBouldersInLowGoal))/float(len(self.autoBouldersInLowGoal)) if float(len(self.autoBouldersInLowGoal)) else 0
-        self.avgAutoBouldersInHighGoal = float(sum(self.autoBouldersInHighGoal))/float(len(self.autoBouldersInHighGoal)) if float(len(self.autoBouldersInHighGoal)) else 0
-        self.avgTeleBouldersInLowGoal = float(sum(self.teleBouldersInLowGoal))/float(len(self.teleBouldersInLowGoal)) if float(len(self.teleBouldersInLowGoal)) else 0
-        self.avgTeleBouldersInHighGoal = float(sum(self.teleBouldersInHighGoal))/float(len(self.teleBouldersInHighGoal)) if float(len(self.teleBouldersInHighGoal)) else 0
-        self.avgTeleBouldersFromLowGoal = float(sum(self.teleBouldersFromLowGoal))/float(len(self.teleBouldersFromLowGoal)) if float(len(self.teleBouldersFromLowGoal)) else 0
-        self.avgTeleBouldersFromHighGoal = float(sum(self.teleBouldersFromHighGoal))/float(len(self.teleBouldersFromHighGoal)) if float(len(self.teleBouldersFromHighGoal)) else 0
-
+       
         self.autoReachesDefencesScore = 0
 
         if self.autoReachesDefences == 1:
@@ -117,11 +111,9 @@ class Entry(object):
 
         if self.autoDefences1 >= 1 and self.autoDefences2 == 0:
              self.autoCrossesDefencesScore = 10
-        elif self.autoDefences2 >= 1 and self.autoDefences == 0:
+        elif self.autoDefences2 >= 1 and self.autoDefences1 == 0:
              self.autoCrossesDefencesScore = 10
         self.autoCrossesDefencesScore = 20 if self.autoDefences1 >= 1 and self.autoDefences2 >= 1 else 0
-
-        self.avgAutoCrossesDefencesScore = float(sum(self.autoCrossesDefencesScore))/float(len(self.autoDefencesScore)) if len(self.autoDefencesScore) else 0
 
         self.autoLowGoal = (self.autoBouldersInLowGoal*5)
         self.autoHighGoal = (self.autoBouldersInHighGoal*10)
@@ -153,9 +145,9 @@ class Entry(object):
 
         self.teleRockWallDamage = 0
         self.teleRoughTerrain = 0
-        if self.Defences4 == "Rock Wall":
+        if self.teleDefences4 == "Rock Wall":
             self.teleRockWallDamage = self.teleDamageCounter5
-        elif self.Defences4 == "Rough Terrain":
+        elif self.teleDefences4 == "Rough Terrain":
             self.teleRoughTerrainDamage = self.teleDamageCounter5
         
         self.teleDC1 = (self.teleDamageCounter1*5) 
@@ -165,8 +157,6 @@ class Entry(object):
         self.teleDC5 = (self.teleDamageCounter5*5)
         
         self.teleDefencesDamageScore = (self.teleDC1 + self.teleDC2 + self.teleDC3 + self.teleDC4 + self.teleDC5)
-
-        self.avgTeleDefencesDamageScore = float(sum(self.teleDefencesDamageScore))/float(len(self.teleDamageDamageScore)) if len(self.teleDefencesDamageScore)  else 0
 
         self.teleLowGoal = (self.teleBouldersInLowGoal*2)
         self.teleHighGoal = (self.teleBouldersInHighGoal*5)
@@ -197,22 +187,19 @@ class Entry(object):
         self.postScaleStateScore = 15 if self.postScaleState == 2 else 0
 
         self.NotAttemptedC = True if self.postChallengeState == 0 else False
-        self.AttemptedC = True if self.postChallenState == 1 else False
-        self.SuccessfulC = True if self.postChallenState == 2 else False
+        self.AttemptedC = True if self.postChallengeState == 1 else False
+        self.SuccessfulC = True if self.postChallengeState == 2 else False
 
         self.NotAttemptedS = True if self.postScaleState == 0 else False
         self.AttemptedS = True if self.postScaleState == 1 else False 
         self.SuccessfulS = True if self.postScaleState == 2 else False
-
-        self.avgTeleChallengeStateSuccessful = float(sum(self.SuccessfulC))/float(len(self.postChallengeState)) if len(self.postChallengeState) else 0
-        self.avgTeleScaleStateSuccessful = float(sum(self.SuccesfulS))/float(len(self.postScaleState)) if len(self.postScaleState) else 0 
 
         self.scoredInAuto = True if self.autoScore > 0 else False
         self.scoredInTele = True if self.teleScore > 0 else False
         self.hasFoul      = True if self.postFouls >0 or self.postTechFouls > 0 else False
 
         self.offensiveScore = (self.autoScore + self.teleScore)
-        self.foulScore = (5*self.postFouls + 5*self.postTechFoul)
+        self.foulScore = (5*self.postFouls + 5*self.postTechFouls)
 
         self.offensive = True if self.offensiveScore > 0 else False
         self.defensive = self.postPlayedDefensively
